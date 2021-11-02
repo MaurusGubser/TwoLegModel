ba04f0ffb4d7f20cfcd370e05401ca8bf2e2dcf4
MaurusGubser <maurus.gubser@gmx.ch>
2021-11-02 13:06:19 +0100

    Added parameters factor_Q, factor_H, factor_kalman.

--- main.py
+++ main.py
@@ -19,23 +19,27 @@ from Plotting import Plotter
 if __name__ == '__main__':
     dt = 0.01
     dim_states = 18
-    dim_observations = 36
+    dim_observations = 20
     leg_constants = np.array([0.5, 0.6, 0.5, 0.6])
     imu_position = np.array([0.34, 0.29, 0.315, 0.33])
     a = np.array([5.6790e-03, 1.0575e+00, -1.2846e-01, -2.4793e-01, 3.6639e-01, -1.8980e-01,
                   5.6790e-01, 9.6320e-02, 2.5362e+00, -3.7986e+00, -7.8163e-02, -8.1819e-01,
                   -4.0705e-11, 5.0517e-03, -1.7762e+00, 3.3158e+00, -2.9528e-01, 5.3581e-01])
 
-    P = 0.1 * np.eye(dim_states)
+    P = 0.0001 * np.eye(dim_states)
 
-    cov_step = 0.01
-    scale_x = 0.01
-    scale_y = 1.0
-    scale_phi = 100.0
+    cov_step = 0.5  # 0.01
+    scale_x = 1.0   # 0.01
+    scale_y = 1.0   # 1.0
+    scale_phi = 1.0     # 100.0
+    factor_Q = 1.0
     sigma_imu_acc = 0.1
     sigma_imu_gyro = 0.01
     sigma_press_velo = 0.1
     sigma_press_acc = 10.0
+    factor_H = 1.0
+
+    factor_kalman = 1.0
 
     my_model = TwoLegModel(dt=dt,
                            dim_states=dim_states,
@@ -48,10 +52,12 @@ if __name__ == '__main__':
                            scale_x=scale_x,
                            scale_y=scale_y,
                            scale_phi=scale_phi,
+                           factor_Q=factor_Q,
                            sigma_imu_acc=sigma_imu_acc,
                            sigma_imu_gyro=sigma_imu_gyro,
                            sigma_press_velo=sigma_press_velo,
-                           sigma_press_acc=sigma_press_acc
+                           sigma_press_acc=sigma_press_acc,
+                           factor_H=factor_H
                            )
 
     my_model_prop = TwoLegModelGuided(dt=dt,
@@ -65,17 +71,20 @@ if __name__ == '__main__':
                                       scale_x=scale_x,
                                       scale_y=scale_y,
                                       scale_phi=scale_phi,
+                                      factor_Q=factor_Q,
                                       sigma_imu_acc=sigma_imu_acc,
                                       sigma_imu_gyro=sigma_imu_gyro,
                                       sigma_press_velo=sigma_press_velo,
-                                      sigma_press_acc=sigma_press_acc
+                                      sigma_press_acc=sigma_press_acc,
+                                      factor_H=factor_H,
+                                      factor_kalman=factor_kalman
                                       )
 
     # simulated data from weto
     path_truth = 'GeneratedData/Normal/truth_normal.dat'
     path_obs = 'GeneratedData/Normal/noised_observations_normal.dat'
     data_reader = DataReader()
-    max_timesteps = 1000
+    max_timesteps = 200
     data_reader.read_states_as_arr(path_truth, max_timesteps=max_timesteps)
     data_reader.read_observations_as_arr(path_obs, max_timesteps=max_timesteps)
     data_reader.prepare_lists()
@@ -94,12 +103,13 @@ if __name__ == '__main__':
     pf.run()
 
     plotter = Plotter(true_states=np.array(x), true_obs=np.array(y), delta_t=0.01)
-    plotter.plot_observations(np.array(pf.hist.X), model=my_model)
-    plotter.plot_particles_trajectories(np.array(pf.hist.X), export_name='trajectories_0')
+    export_name = 'pf_Q1_H1000'
+    plotter.plot_observations(np.array(pf.hist.X), model=my_model, export_name=export_name)
+    plotter.plot_particles_trajectories(np.array(pf.hist.X), export_name=export_name)
     particles_mean = np.array([m['mean'] for m in pf.summaries.moments])
     particles_var = np.array([m['var'] for m in pf.summaries.moments])
     plotter.plot_particle_moments(particles_mean=particles_mean, particles_var=particles_var,
-                                  X_hist=np.array(pf.hist.X), export_name='trajectories_0')
+                                  X_hist=np.array(pf.hist.X), export_name=export_name)
 
     """
     # compare MC and QMC method
@@ -110,8 +120,8 @@ if __name__ == '__main__':
     """
 
     # smoothing
-    smooth_trajectories = pf.hist.backward_sampling(5)
-    plotter.plot_samples_detail(samples=np.array(smooth_trajectories), export_name='trajectories_10_Q1_H001_Prop100')
+    smooth_trajectories = pf.hist.backward_sampling(5, linear_cost=False)
+    plotter.plot_smoothed_trajectories(samples=np.array(smooth_trajectories), export_name=export_name)
 
     """
     # learning parameters
