9e9aeabdfba47ebffa99b155e1f314b728cf0c5a
MaurusGubser <maurus.gubser@gmx.ch>
2021-11-25 16:12:03 +0100

    Added function set_prior.

--- main.py
+++ main.py
@@ -14,12 +14,41 @@ from ReadData import DataReaderWriter
 from TwoLegSMCModel import TwoLegModel
 from Plotting import Plotter
 
+
+def set_prior(add_Q, add_H, add_legs, add_imus):
+    prior = {}
+    if add_Q:
+        prior_Q = {'scale_x': dists.Uniform(40.0, 200.0),
+                   'scale_y': dists.Uniform(40.0, 200.0),
+                   'scale_phi': dists.Uniform(50.0, 500.0)}
+        prior.update(prior_Q)
+    if add_H:
+        prior_H = {'sigma_imu_acc': dists.LinearD(dists.InvGamma(3.0, 2.0), a=0.1, b=0.0),
+                   'sigma_imu_gyro': dists.LinearD(dists.InvGamma(3.0, 2.0), a=0.01, b=0.0),
+                   'sigma_press_velo': dists.LinearD(dists.InvGamma(3.0, 2.0), a=0.1, b=0.0),
+                   'sigma_press_acc': dists.LinearD(dists.InvGamma(3.0, 2.0), a=1000.0, b=0.0)}
+        prior.update(prior_H)
+    if add_legs:
+        dist_femur = dists.Uniform(0.4, 0.6)
+        dist_fibula = dists.Uniform(0.5, 0.7)
+        prior_legs = {'len_legs': dists.IndepProd(*[dist_femur, dist_fibula, dist_femur, dist_fibula])}
+        prior.update(prior_legs)
+    if add_imus:
+        dist_imu0 = dists.Uniform(0.29, 0.39)
+        dist_imu1 = dists.Uniform(0.24, 0.34)
+        dist_imu2 = dists.Uniform(0.265, 0.365)
+        dist_imu3 = dists.Uniform(0.28, 0.38)
+        prior_imus = {'pos_imus': [dist_imu0, dist_imu1, dist_imu2, dist_imu3]}
+        prior.update(prior_imus)
+    return prior, dists.StructDist(prior)
+
+
 if __name__ == '__main__':
     dt = 0.01
     dim_states = 18
-    dim_observations = 36
-    leg_constants = np.array([0.5, 0.6, 0.5, 0.6])
-    imu_position = np.array([0.34, 0.29, 0.315, 0.33])
+    dim_observations = 20
+    length_legs = np.array([0.5, 0.6, 0.5, 0.6])
+    position_imus = np.array([0.34, 0.29, 0.315, 0.33])
     a = np.array([5.6790e-03, 1.0575e+00, -1.2846e-01, -2.4793e-01, 3.6639e-01, -1.8980e-01,
                   5.6790e-01, 9.6320e-02, 2.5362e+00, -3.7986e+00, -7.8163e-02, -8.1819e-01,
                   -4.0705e-11, 5.0517e-03, -1.7762e+00, 3.3158e+00, -2.9528e-01, 5.3581e-01])
@@ -42,8 +71,8 @@ if __name__ == '__main__':
     my_model = TwoLegModel(dt=dt,
                            dim_states=dim_states,
                            dim_observations=dim_observations,
-                           leg_constants=leg_constants,
-                           imu_position=imu_position,
+                           len_legs=length_legs,
+                           pos_imus=position_imus,
                            a=a,
                            P=P,
                            cov_step=cov_step,
@@ -64,7 +93,7 @@ if __name__ == '__main__':
     path_truth = 'GeneratedData/Normal/truth_normal.dat'    # 'GeneratedData/sandbox/truth_sandbox.dat'
     path_obs = 'GeneratedData/Normal/noised_observations_normal.dat'    # 'GeneratedData/sandbox/noised_observations_sandbox.dat'
     data_reader = DataReaderWriter()
-    max_timesteps = 1000
+    max_timesteps = 100
     data_reader.read_states_as_arr(path_truth, max_timesteps=max_timesteps)
     data_reader.read_observations_as_arr(path_obs, max_timesteps=max_timesteps)
     data_reader.prepare_lists()
@@ -80,7 +109,7 @@ if __name__ == '__main__':
     fk_boot = ssm.Bootstrap(ssm=my_model, data=y)
     fk_guided = ssm.GuidedPF(ssm=my_model, data=y)
     pf = particles.SMC(fk=fk_guided, N=nb_particles, ESSrmin=0.5, store_history=True, collect=[Moments()], verbose=True)
-
+    """
     # filter and plot
     start_user, start_process = time.time(), time.process_time()
     pf.run()
@@ -97,7 +126,7 @@ if __name__ == '__main__':
     plotter.plot_ESS(pf.summaries.ESSs)
     plotter.plot_particle_moments(particles_mean=particles_mean, particles_var=particles_var,
                                   X_hist=None, export_name=export_name)  # X_hist = np.array(pf.hist.X)
-
+    """
     """
     # compare MC and QMC method
     results = particles.multiSMC(fk=fk_guided, N=100, nruns=30, qmc={'SMC': False, 'SQMC': True})
@@ -105,25 +134,20 @@ if __name__ == '__main__':
     sb.boxplot(x=[r['output'].logLt for r in results], y=[r['qmc'] for r in results])
     plt.show()
     """
-
+    """
     # smoothing
     smooth_trajectories = pf.hist.backward_sampling(5, linear_cost=False)
     data_reader.export_trajectory(np.array(smooth_trajectories), dt, export_name)
     plotter.plot_smoothed_trajectories(samples=np.array(smooth_trajectories), export_name=export_name)
-
     """
     # learning parameters
-    prior_dict = {'scale_x': dists.Uniform(20.0, 200.0),
-                  'scale_y': dists.Uniform(20.0, 200.0),
-                  'scale_phi': dists.Uniform(50.0, 500.0)}
-    prior_dict = {'sigma_imu_acc': dists.LinearD(dists.InvGamma(3.0, 2.0), a=0.1, b=0.0),
-                  'sigma_imu_gyro': dists.LinearD(dists.InvGamma(3.0, 2.0), a=0.01, b=0.0),
-                  'sigma_press_velo': dists.LinearD(dists.InvGamma(3.0, 2.0), a=0.1, b=0.0),
-                  'sigma_press_acc': dists.LinearD(dists.InvGamma(3.0, 2.0), a=1000.0, b=0.0)}
-
-    my_prior = dists.StructDist(prior_dict)
-    pmmh = mcmc.PMMH(ssm_cls=TwoLegModelGuided, prior=my_prior, fk_cls=ssm.Bootstrap, data=y, Nx=100, niter=200,
-                     verbose=10)
+    add_Q = True
+    add_H = False
+    add_legs = False
+    add_imu = False
+    prior_dict, my_prior = set_prior(add_Q, add_H, add_legs, add_imu)
+    pmmh = mcmc.PMMH(ssm_cls=TwoLegModel, prior=my_prior, fk_cls=ssm.Bootstrap, data=y, Nx=100, niter=200,
+                     verbose=0)
     start_user, start_process = time.time(), time.process_time()
     pmmh.run()  # Warning: takes a few seconds
     end_user, end_process = time.time(), time.process_time()
@@ -135,11 +159,3 @@ if __name__ == '__main__':
         sb.distplot(pmmh.chain.theta[param][burnin:], 10)
         plt.title(param)
     plt.show()
-
-    for i, param in enumerate(prior_dict.keys()):  # loop over mu, theta, rho
-        plt.subplot(2, 2, i + 1)
-        plt.plot(pmmh.chain.theta[param])
-        plt.xlabel('iter')
-        plt.ylabel(param)
-    plt.show()
-    """
