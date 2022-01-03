b1765e5e897f9ea4d5457b1cef42d44b5d696b05
MaurusGubser <maurus.gubser@gmx.ch>
2022-01-03 12:42:15 +0100

    Changed default parameters for parameter learning.

--- TwoLegSMCModel.py
+++ TwoLegSMCModel.py
@@ -34,13 +34,13 @@ class TwoLegModel(ssm.StateSpaceModel):
                  scale_x=10000.0,
                  scale_y=1000.0,
                  scale_phi=10000000.0,
-                 factor_Q=0.1,     # 1.0
+                 factor_Q=1.0,     # 1.0
                  diag_Q=False,
                  sigma_imu_acc=0.001,
                  sigma_imu_gyro=0.0001,
                  sigma_press_velo=0.001,
                  sigma_press_acc=0.01,
-                 factor_H=1.0,  # 1.0
+                 factor_H=10.0,  # 1.0
                  factor_proposal=1.2):  # 1.2
         super().__init__()
         self.dt = dt
