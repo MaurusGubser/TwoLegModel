df50526b6dc4cad1152dba1f20b3e47a3ea3ff76
MaurusGubser <maurus.gubser@gmx.ch>
2022-02-02 17:53:33 +0100

    Added log likelihood plots to Plotter class.

--- Plotter.py
+++ Plotter.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sb
 import os
 
 
@@ -28,6 +29,7 @@ class Plotter:
         self.contact_right = np.zeros((nb_steps_obs, 1))
         self.set_contacts()
         self.delta_t = delta_t
+        self.t_vals = np.linspace(0.0, self.nb_steps * self.delta_t, self.nb_steps)
 
     def set_export_path(self):
         if self.export_name:
@@ -61,7 +63,7 @@ class Plotter:
         nb_graphs = min(nb_particles, 5)
         nb_axes = 3
         nb_figures = int(np.ceil(self.dim_states / nb_axes))
-        t_vals = np.linspace(0.0, self.nb_steps * self.delta_t, self.nb_steps)
+        # t_vals = np.linspace(0.0, self.nb_steps * self.delta_t, self.nb_steps)
         if self.dim_states == 18:
             state_names = ['$x_H$', '$y_H$', r'$\varphi_0$', r'$\varphi_1$', r'$\varphi_2$', r'$\varphi_3$',
                            r'$\dot x_H$', r'$\dot y_H$', r'$\dot \varphi_0$', r'$\dot \varphi_1$', r'$\dot \varphi_2$',
@@ -86,21 +88,24 @@ class Plotter:
                     break
                 axs[j].grid(axis='both')
                 for k in range(0, nb_graphs):
-                    axs[j].plot(t_vals, X_hist[:, k, nb_axes * i + j], label='Sample {}'.format(k), lw=1)
-                axs[j].plot(t_vals, self.true_states[:, :, nb_axes * i + j], label='truth', lw=1.5, color='green')
+                    axs[j].plot(self.t_vals, X_hist[:, k, nb_axes * i + j], label='Sample {}'.format(k), lw=1)
+                axs[j].plot(self.t_vals, self.true_states[:, :, nb_axes * i + j], label='truth', lw=1.5, color='green')
                 axs[j].set_title(state_names[nb_axes * i + j])
                 axs[j].legend()
                 if state_names[nb_axes * i + j] == r'$y_H$':
-                    axs[j % nb_axes].plot(t_vals, self.contact_left + 1.0, label='Contact left', color='red', lw=1.5)
-                    axs[j % nb_axes].plot(t_vals, self.contact_right + 1.0, label='Contact right', color='orange',
+                    axs[j % nb_axes].plot(self.t_vals, self.contact_left + 1.0, label='Contact left', color='red',
+                                          lw=1.5)
+                    axs[j % nb_axes].plot(self.t_vals, self.contact_right + 1.0, label='Contact right', color='orange',
                                           lw=1.5)
                 elif state_names[nb_axes * i + j] == r'$\dot x_H$':
-                    axs[j % nb_axes].plot(t_vals, self.contact_left + 0.6, label='Contact left', color='red', lw=1.5)
-                    axs[j % nb_axes].plot(t_vals, self.contact_right + 0.6, label='Contact right', color='orange',
+                    axs[j % nb_axes].plot(self.t_vals, self.contact_left + 0.6, label='Contact left', color='red',
+                                          lw=1.5)
+                    axs[j % nb_axes].plot(self.t_vals, self.contact_right + 0.6, label='Contact right', color='orange',
                                           lw=1.5)
                 else:
-                    axs[j % nb_axes].plot(t_vals, self.contact_left, label='Contact left', color='red', lw=1.5)
-                    axs[j % nb_axes].plot(t_vals, self.contact_right, label='Contact right', color='orange', lw=1.5)
+                    axs[j % nb_axes].plot(self.t_vals, self.contact_left, label='Contact left', color='red', lw=1.5)
+                    axs[j % nb_axes].plot(self.t_vals, self.contact_right, label='Contact right', color='orange',
+                                          lw=1.5)
             fig.suptitle('{} particle trajectories'.format(nb_graphs))
             fig.tight_layout()
             fig_list.append(fig)
@@ -120,7 +125,7 @@ class Plotter:
         nb_graphs = min(nb_samples, 5)
         nb_axes = 3
         nb_figures = int(np.ceil(self.dim_states / nb_axes))
-        t_vals = np.linspace(0.0, self.nb_steps * self.delta_t, self.nb_steps)
+        # t_vals = np.linspace(0.0, self.nb_steps * self.delta_t, self.nb_steps)
         if self.dim_states == 18:
             state_names = ['$x_H$', '$y_H$', r'$\varphi_0$', r'$\varphi_1$', r'$\varphi_2$', r'$\varphi_3$',
                            r'$\dot x_H$', r'$\dot y_H$', r'$\dot \varphi_0$', r'$\dot \varphi_1$', r'$\dot \varphi_2$',
@@ -145,20 +150,24 @@ class Plotter:
                     break
                 axs[j].grid(axis='both')
                 for k in range(0, nb_graphs):
-                    axs[j % nb_axes].plot(t_vals, samples[:, k, nb_axes * i + j], label='Sample {}'.format(k), lw=1)
-                axs[j % nb_axes].plot(t_vals, self.true_states[:, :, nb_axes * i + j], label='truth', lw=1.5,
+                    axs[j % nb_axes].plot(self.t_vals, samples[:, k, nb_axes * i + j], label='Sample {}'.format(k),
+                                          lw=1)
+                axs[j % nb_axes].plot(self.t_vals, self.true_states[:, :, nb_axes * i + j], label='truth', lw=1.5,
                                       color='green')
                 if state_names[nb_axes * i + j] == r'$y_H$':
-                    axs[j % nb_axes].plot(t_vals, self.contact_left + 1.0, label='Contact left', color='red', lw=1.5)
-                    axs[j % nb_axes].plot(t_vals, self.contact_right + 1.0, label='Contact right', color='orange',
+                    axs[j % nb_axes].plot(self.t_vals, self.contact_left + 1.0, label='Contact left', color='red',
+                                          lw=1.5)
+                    axs[j % nb_axes].plot(self.t_vals, self.contact_right + 1.0, label='Contact right', color='orange',
                                           lw=1.5)
                 elif state_names[nb_axes * i + j] == r'$\dot x_H$':
-                    axs[j % nb_axes].plot(t_vals, self.contact_left + 0.6, label='Contact left', color='red', lw=1.5)
-                    axs[j % nb_axes].plot(t_vals, self.contact_right + 0.6, label='Contact right', color='orange',
+                    axs[j % nb_axes].plot(self.t_vals, self.contact_left + 0.6, label='Contact left', color='red',
+                                          lw=1.5)
+                    axs[j % nb_axes].plot(self.t_vals, self.contact_right + 0.6, label='Contact right', color='orange',
                                           lw=1.5)
                 else:
-                    axs[j % nb_axes].plot(t_vals, self.contact_left, label='Contact left', color='red', lw=1.5)
-                    axs[j % nb_axes].plot(t_vals, self.contact_right, label='Contact right', color='orange', lw=1.5)
+                    axs[j % nb_axes].plot(self.t_vals, self.contact_left, label='Contact left', color='red', lw=1.5)
+                    axs[j % nb_axes].plot(self.t_vals, self.contact_right, label='Contact right', color='orange',
+                                          lw=1.5)
                 axs[j % nb_axes].set_title(state_names[nb_axes * i + j])
                 axs[j % nb_axes].legend()
             fig.suptitle('Smoothed samples')
@@ -173,7 +182,7 @@ class Plotter:
     def plot_particle_moments(self, particles_mean, particles_var):
         nb_axes = 3
         nb_figures = int(np.ceil(self.dim_states / nb_axes))
-        t_vals = np.linspace(0.0, self.nb_steps * self.delta_t, self.nb_steps)
+        # t_vals = np.linspace(0.0, self.nb_steps * self.delta_t, self.nb_steps)
         particles_sd = np.sqrt(particles_var)
         if self.dim_states == 18:
             state_names = ['$x_H$', '$y_H$', r'$\varphi_0$', r'$\varphi_1$', r'$\varphi_2$', r'$\varphi_3$',
@@ -197,22 +206,25 @@ class Plotter:
                 if i * nb_axes + j > self.dim_states - 1:
                     break
                 axs[j].grid(axis='both')
-                axs[j].plot(t_vals, particles_mean[:, nb_axes * i + j], label='Particle mean', color='blue')
-                axs[j].fill_between(t_vals, particles_mean[:, nb_axes * i + j] - particles_sd[:, nb_axes * i + j],
+                axs[j].plot(self.t_vals, particles_mean[:, nb_axes * i + j], label='Particle mean', color='blue')
+                axs[j].fill_between(self.t_vals, particles_mean[:, nb_axes * i + j] - particles_sd[:, nb_axes * i + j],
                                     particles_mean[:, nb_axes * i + j] + particles_sd[:, nb_axes * i + j], alpha=0.2,
                                     color='blue')
-                axs[j].plot(t_vals, self.true_states[:, :, nb_axes * i + j], label='Truth', color='green', lw=1.5)
+                axs[j].plot(self.t_vals, self.true_states[:, :, nb_axes * i + j], label='Truth', color='green', lw=1.5)
                 if state_names[nb_axes * i + j] == r'$y_H$':
-                    axs[j % nb_axes].plot(t_vals, self.contact_left + 1.0, label='Contact left', color='red', lw=1.5)
-                    axs[j % nb_axes].plot(t_vals, self.contact_right + 1.0, label='Contact right', color='orange',
+                    axs[j % nb_axes].plot(self.t_vals, self.contact_left + 1.0, label='Contact left', color='red',
+                                          lw=1.5)
+                    axs[j % nb_axes].plot(self.t_vals, self.contact_right + 1.0, label='Contact right', color='orange',
                                           lw=1.5)
                 elif state_names[nb_axes * i + j] == r'$\dot x_H$':
-                    axs[j % nb_axes].plot(t_vals, self.contact_left + 0.6, label='Contact left', color='red', lw=1.5)
-                    axs[j % nb_axes].plot(t_vals, self.contact_right + 0.6, label='Contact right', color='orange',
+                    axs[j % nb_axes].plot(self.t_vals, self.contact_left + 0.6, label='Contact left', color='red',
+                                          lw=1.5)
+                    axs[j % nb_axes].plot(self.t_vals, self.contact_right + 0.6, label='Contact right', color='orange',
                                           lw=1.5)
                 else:
-                    axs[j % nb_axes].plot(t_vals, self.contact_left, label='Contact left', color='red', lw=1.5)
-                    axs[j % nb_axes].plot(t_vals, self.contact_right, label='Contact right', color='orange', lw=1.5)
+                    axs[j % nb_axes].plot(self.t_vals, self.contact_left, label='Contact left', color='red', lw=1.5)
+                    axs[j % nb_axes].plot(self.t_vals, self.contact_right, label='Contact right', color='orange',
+                                          lw=1.5)
                 axs[j].set_title(state_names[nb_axes * i + j])
                 axs[j].legend()
 
@@ -256,7 +268,7 @@ class Plotter:
         else:
             raise ValueError('Observation dimension has to be 20 or 36; got {} instead.'.format(self.dim_observations))
         _, _, nb_observations = obs.shape
-        t_vals = np.linspace(0.0, nb_steps * self.delta_t, nb_steps)
+        # t_vals = np.linspace(0.0, nb_steps * self.delta_t, nb_steps)
         nb_axes = 3
         nb_figures = int(np.ceil(nb_observations / nb_axes))
         for i in range(0, nb_figures):
@@ -266,8 +278,9 @@ class Plotter:
                     break
                 axs[j].grid(axis='both')
                 for k in range(0, nb_graphs):
-                    axs[j].plot(t_vals, obs[:, k, i * nb_axes + j], label='Sample {}'.format(k), lw=1)
-                axs[j].plot(t_vals, true_obs[:, :, i * nb_axes + j], label='True observation', color='green', lw=1.5)
+                    axs[j].plot(self.t_vals, obs[:, k, i * nb_axes + j], label='Sample {}'.format(k), lw=1)
+                axs[j].plot(self.t_vals, true_obs[:, :, i * nb_axes + j], label='True observation', color='green',
+                            lw=1.5)
                 axs[j].legend()
                 axs[j].set_title(obs_names[i * nb_axes + j])
             fig.tight_layout()
@@ -277,15 +290,15 @@ class Plotter:
         return None
 
     def plot_ESS(self, ESS):
-        t_vals = np.linspace(0.0, self.nb_steps * self.delta_t, self.nb_steps)
+        # t_vals = np.linspace(0.0, self.nb_steps * self.delta_t, self.nb_steps)
         window_avg = np.ones(10) / 10.0
         moving_avg = np.convolve(ESS, window_avg, 'same')
         fig = plt.figure(figsize=(12, 8))
         plt.grid(axis='both')
-        plt.plot(t_vals, ESS, label='ESS')
-        plt.plot(t_vals, moving_avg, label='ESS Moving Avg')
-        plt.plot(t_vals, self.contact_left, label='Contact left', color='red', lw=1.5)
-        plt.plot(t_vals, self.contact_right, label='Contact right', color='orange', lw=1.5)
+        plt.plot(self.t_vals, ESS, label='ESS')
+        plt.plot(self.t_vals, moving_avg, label='ESS Moving Avg')
+        plt.plot(self.t_vals, self.contact_left, label='Contact left', color='red', lw=1.5)
+        plt.plot(self.t_vals, self.contact_right, label='Contact right', color='orange', lw=1.5)
         fig.suptitle('Essential sample size')
         plt.legend()
         if self.export_path is not None:
@@ -293,11 +306,11 @@ class Plotter:
         plt.show()
         return None
 
-    def plot_logLts(self, logLts):
-        t_vals = np.linspace(0.0, self.nb_steps * self.delta_t, self.nb_steps)
+    def plot_logLts_one_run(self, logLts):
+        # t_vals = np.linspace(0.0, self.nb_steps * self.delta_t, self.nb_steps)
         fig = plt.figure(figsize=(12, 8))
         plt.grid(axis='both')
-        plt.plot(t_vals, logLts, label=r'$\log(p(y_{t}|y_{0:t-1}))$')
+        plt.plot(self.t_vals, logLts, label=r'$\log(p(y_{t}|y_{0:t-1}))$')
         fig.suptitle('Log likelihood factors')
         plt.legend()
         if self.export_path is not None:
@@ -309,3 +322,37 @@ class Plotter:
         residuals = np.empty_like(observations)
         residuals = np.abs(self.true_obs - observations)
         return residuals
+
+    def plot_logLts_multiple_runs(self, output_multismc, nb_particles, nb_runs, t_start):
+        for N in nb_particles:
+            loglts = np.array([r['output'].summaries.logLts for r in output_multismc if r['N'] == N])
+            mean_loglts = np.mean(loglts, axis=0)
+            sd_loglots = np.std(loglts, axis=0)
+            fig = plt.figure(figsize=(12, 8))
+            plt.grid(axis='both')
+            plt.plot(self.t_vals, mean_loglts, label='Number particles='.format(N))
+            plt.fill_between(self.t_vals, mean_loglts - sd_loglots, mean_loglts + sd_loglots, alpha=0.5)
+            plt.xlabel('t')
+            plt.ylabel('$p(y_{t}|y_{0:t-1})$')
+            fig.suptitle('Mean and var over {} runs'.format(nb_runs))
+            if self.export_path:
+                plt.savefig(self.export_path + '/Likelihoods_mean_var_nbparticles' + str(N) + '.pdf')
+
+        fig = plt.figure(figsize=(12, 8))
+        sb.boxplot(x=[np.sum(r['output'].summaries.logLts[t_start:]) for r in output_multismc],
+                   y=[str(r['N']) for r in output_multismc])
+        plt.xlabel('$p(y_{0:T})$')
+        plt.ylabel('Number of particles')
+        fig.suptitle('Log likelihood over {} runs of {} timesteps'.format(nb_runs, self.nb_steps))
+        if self.export_path:
+            plt.savefig(self.export_path + '/Likelihood_Boxplot.pdf')
+
+        fig = plt.figure(figsize=(12, 8))
+        sb.histplot(x=[np.sum(r['output'].summaries.logLts[t_start:]) for r in output_multismc],
+                    hue=[str(r['N']) for r in output_multismc], multiple='dodge')
+        plt.xlabel('Bins of $p(y_{0:T})$')
+        fig.suptitle('Histogram of log likelihood over {} runs of {} timesteps'.format(nb_runs, self.nb_steps))
+        if self.export_path:
+            plt.savefig(self.export_path + '/Likelihood_Histogram.pdf')
+        plt.show()
+        return None
