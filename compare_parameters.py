de69fe655050e421009fa115aec97f7658e61697
MaurusGubser <maurus.gubser@gmx.ch>
2022-03-24 19:19:16 +0100

    Added boxplot for non-truncated likelihood.

--- compare_parameters.py
+++ compare_parameters.py
@@ -33,16 +33,22 @@ def compare_parameters(fk_models, nb_timesteps, nb_particles, nb_runs, t_start, 
     end_user, end_process = time.time(), time.process_time()
     print('Time user {:.1f}s; time processor {:.1f}s'.format(end_user - start_user, end_process - start_process))
 
+    logLts = [r['output'].summaries.logLts[-1] for r in results]
+    mean, var = np.mean(logLts, axis=0), np.var(logLts, axis=0)
+    print('N={:.5E}, Mean loglhd={:.5E}, Variance loglhd={:.5E}'.format(N, mean, var))
+    plt.figure(figsize=(15, 8))
+    sb.boxplot(x=logLts, y=[r['fk'] for r in results])
+    plt.title('Boxplots for likelihood')
+
     logLts_truncated = [r['output'].summaries.logLts[-1] - r['output'].summaries.logLts[t_start] for r in results]
     mean, var = np.mean(logLts_truncated, axis=0), np.var(logLts_truncated, axis=0)
     print('N={:.5E}, Mean loglhd truncated={:.5E}, Variance loglhd truncated={:.5E}'.format(N, mean, var))
-    sb.boxplot(x=[r['output'].summaries.logLts[-1] - r['output'].summaries.logLts[t_start] for r in results],
-               y=[r['fk'] for r in results])
+    plt.figure(figsize=(15, 8))
+    sb.boxplot(x=logLts_truncated, y=[r['fk'] for r in results])
     plt.title('Boxplots for truncated likelihood')
-    plt.show()
 
     t_vals = np.arange(nb_timesteps)
-    fig = plt.figure(figsize=(15, 8))
+    plt.figure(figsize=(15, 8))
     for fk_model in fk_models.keys():
         logLts = np.array([r['output'].summaries.logLts for r in results if r['fk'] == fk_model])
         mean, std = np.mean(logLts, axis=0), np.std(logLts, axis=0)
@@ -59,20 +65,20 @@ def compare_parameters(fk_models, nb_timesteps, nb_particles, nb_runs, t_start, 
 if __name__ == '__main__':
     # ---------------------------- data ----------------------------
     generation_type = 'Missingdata005'
-    nb_timesteps = 1000
+    nb_timesteps = 100
     dim_obs = 20  # 20 or 36
     x, y = prepare_data(generation_type, nb_timesteps, dim_obs)
 
     # ---------------------------- model ----------------------------
     # ! model works with parameters defined below; other parameters are chosen as the standard params in TwoLegModelSMC.py
     dt = 0.01
-    N = 1000
-    t_start = 500
-    nb_runs = 20
+    N = 100
+    t_start = 50
+    nb_runs = 2
     show_fig = False
     export_name = 'pos_imu0'
 
-    parameters = [{'pos_imu0': 0.20}, {'pos_imu0': 0.25}, {}, {'pos_imu0': 0.35}, {'pos_imu0': 0.4}, {'pos_imu0': 0.45}]
+    parameters = [{'pos_imu0': 0.20}, {'pos_imu0': 0.25}]   #, {}, {'pos_imu0': 0.35}, {'pos_imu0': 0.4}, {'pos_imu0': 0.45}]
     fk_models = {}
     for param in parameters:
         fk_models[str(param)] = ssm.GuidedPF(ssm=TwoLegModel(**param), data=y)
