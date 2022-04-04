b697a89d52e882b6340306da9a1a2e04d2f97c75
MaurusGubser <maurus.gubser@gmx.ch>
2022-04-04 12:15:26 +0200

    Added factor_proposal to parameters.

--- compare_parameters.py
+++ compare_parameters.py
@@ -12,21 +12,6 @@ from TwoLegSMCModel import TwoLegModel
 from Plotter import Plotter
 
 
-def prepare_data(generation_type, max_timesteps, dim_observations):
-    path_truth = 'GeneratedData/' + generation_type + '/truth.dat'
-    path_obs = 'GeneratedData/' + generation_type + '/noised_observations.dat'
-    data_reader = DataReaderWriter()
-    data_reader.read_states_as_arr(path_truth, max_timesteps=max_timesteps)
-    data_reader.read_observations_as_arr(path_obs, max_timesteps=max_timesteps)
-    data_reader.prepare_lists()
-    states = data_reader.states_list
-    observations = data_reader.observations_list
-    if dim_observations == 20:
-        observations = [obs[:, (0, 1, 5, 6, 7, 11, 12, 13, 17, 18, 19, 23, 24, 25, 27, 28, 30, 31, 33, 34)] for obs in
-                        observations]
-    return states, observations
-
-
 def compare_parameters(fk_models, true_states, data, dt, nb_particles, nb_runs, t_start, show_fig, export_name=None):
     start_user, start_process = time.time(), time.process_time()
     results = particles.multiSMC(fk=fk_models, N=nb_particles, nruns=nb_runs, collect=[LogLts()], nprocs=-1)
@@ -47,7 +32,8 @@ if __name__ == '__main__':
     generation_type = 'Missingdata005'
     nb_timesteps = 1000
     dim_obs = 20  # 20 or 36
-    x, y = prepare_data(generation_type, nb_timesteps, dim_obs)
+    data_reader = DataReaderWriter()
+    x, y = data_reader.get_data_as_lists(generation_type, nb_timesteps, dim_obs)
 
     # ---------------------------- model ----------------------------
     # ! model works with parameters defined below; other parameters are chosen as the standard params in TwoLegModelSMC.py
@@ -56,7 +42,7 @@ if __name__ == '__main__':
     t_start = 500
     nb_runs = 50
     show_fig = True
-    params = 'pos_imu0'
+    params = 'factor_proposal'
     export_name = 'MultiRun_{}_steps{}_N{}_nbruns{}_tstart{}_params{}'.format(
         generation_type,
         nb_timesteps,
@@ -65,8 +51,8 @@ if __name__ == '__main__':
         t_start,
         params)
 
-    parameters = [{'pos_imu0': 0.15}, {'pos_imu0': 0.20}, {'pos_imu0': 0.25}, {}, {'pos_imu0': 0.35}, {'pos_imu0': 0.4},
-                  {'pos_imu0': 0.45}]
+    parameters = [{'factor_proposal': 1.0}, {'factor_proposal': 1.05}, {'factor_proposal': 1.1}, {'factor_proposal': 1.15}, {'factor_proposal': 1.2}, {'factor_proposal': 1.25}, {'factor_proposal': 1.3}, {'factor_proposal': 1.35}, {'factor_proposal': 1.4}]
+    # parameters = [{'pos_imu0': 0.15}, {'pos_imu0': 0.20}, {'pos_imu0': 0.25}, {}, {'pos_imu0': 0.35}, {'pos_imu0': 0.4}, {'pos_imu0': 0.45}]
     # parameters = [{'pos_imu0': 0.15, 'pos_imu2': 0.15}, {'pos_imu0': 0.2, 'pos_imu2': 0.2}, {'pos_imu0': 0.25, 'pos_imu2': 0.25}, {}, {'pos_imu0': 0.35, 'pos_imu2': 0.35}, {'pos_imu0': 0.4, 'pos_imu2': 0.4}, {'pos_imu0': 0.45, 'pos_imu2': 0.45}]
     # parameters = [{'alpha_0': -0.2}, {'alpha_0': -0.1}, {'alpha_0': 0.0}, {'alpha_0': 0.1}, {'alpha_0': 0.2}, {'alpha_0': 0.3}]
     # parameters = [{'alpha_0': -0.2, 'alpha_2': -0.2}, {'alpha_0': -0.1, 'alpha_2': -0.1}, {'alpha_0': 0.0, 'alpha_2': 0.0}, {'alpha_0': 0.1, 'alpha_2': 0.1}, {'alpha_0': 0.2, 'alpha_2': 0.2}, {'alpha_0': 0.3, 'alpha_2': 0.3}]
