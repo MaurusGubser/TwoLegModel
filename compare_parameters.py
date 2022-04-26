3181655332e84d65a08344ff356f3d60ab82b00b
MaurusGubser <maurus.gubser@gmx.ch>
2022-04-26 19:29:31 +0200

    Added comments

--- compare_parameters.py
+++ compare_parameters.py
@@ -21,26 +21,25 @@ def compare_parameters(fk_models, true_states, data, dt, nb_particles, nb_runs, 
 
     plotter = Plotter(np.array(true_states), np.array(data), dt, export_name, show_fig)
     plotter.plot_likelihood_parameters(results, fk_models.keys(), t_trunc)
-
     return None
 
 
 if __name__ == '__main__':
     # ---------------------------- data ----------------------------
     generation_type = 'Missingdata005'  # name of data subfolder
-    nb_timesteps = 1000
+    nb_timesteps = 100
     dim_obs = 20  # 20 or 36
     data_reader = DataReaderWriter()
     x, y = data_reader.get_data_as_lists(generation_type, nb_timesteps, dim_obs)
 
     # ---------------------------- model ----------------------------
-    # ! model works with parameters defined below; other parameters are chosen as the standard params in TwoLegModelSMC.py
+    # model works with parameters defined below; other parameters are chosen as the default params in TwoLegModelSMC
     dt = 0.01
-    N = 1000
-    t_trunc = 500
-    nb_runs = 30
+    N = 10
+    t_trunc = 50
+    nb_runs = 10
     show_fig = True
-    params = 'pos_imu'
+    params = 'alphas'
     export_name = 'MultiRun_{}_steps{}_N{}_nbruns{}_tstart{}_params{}'.format(
         generation_type,
         nb_timesteps,
@@ -49,15 +48,16 @@ if __name__ == '__main__':
         t_trunc,
         params)
 
+    # suggestions for parameters, for which likelihood should be compared
     # parameters = [{'femur_left': 0.3, 'femur_right': 0.3}, {'femur_left': 0.4, 'femur_right': 0.4}, {'femur_left': 0.5, 'femur_right': 0.5}, {'femur_left': 0.6, 'femur_right': 0.6}, {'femur_left': 0.7, 'femur_right': 0.7}]
     # parameters = [{'femur_left': 0.3}, {'femur_left': 0.4}, {'femur_left': 0.5}, {'femur_left': 0.6}, {'femur_left': 0.7}]
     # parameters = [{'factor_proposal': 1.0}, {'factor_proposal': 1.05}, {'factor_proposal': 1.1}, {'factor_proposal': 1.15}, {'factor_proposal': 1.2}, {'factor_proposal': 1.25}, {'factor_proposal': 1.3}, {'factor_proposal': 1.35}, {'factor_proposal': 1.4}]
     # parameters = [{'pos_imu0': 0.15}, {'pos_imu0': 0.20}, {'pos_imu0': 0.25}, {}, {'pos_imu0': 0.35}, {'pos_imu0': 0.4}, {'pos_imu0': 0.45}]
     # parameters = [{'pos_imu0': 0.15, 'pos_imu2': 0.15}, {'pos_imu0': 0.2, 'pos_imu2': 0.2}, {'pos_imu0': 0.25, 'pos_imu2': 0.25}, {}, {'pos_imu0': 0.35, 'pos_imu2': 0.35}, {'pos_imu0': 0.4, 'pos_imu2': 0.4}, {'pos_imu0': 0.45, 'pos_imu2': 0.45}]
-    parameters = [{'alpha0': -0.2}, {'alpha0': -0.1}, {'alpha0': 0.0}, {'alpha0': 0.1}, {'alpha0': 0.2}, {'alpha0': 0.3}]
-    # parameters = [{'alpha0': -0.2, 'alpha2': -0.2}, {'alpha0': -0.1, 'alpha2': -0.1}, {'alpha0': 0.0, 'alpha2': 0.0}, {'alpha0': 0.1, 'alpha2': 0.1}, {'alpha0': 0.2, 'alpha2': 0.2}, {'alpha0': 0.3, 'alpha2': 0.3}]
+    # parameters = [{'alpha0': -0.2}, {'alpha0': -0.1}, {'alpha0': 0.0}, {'alpha0': 0.1}, {'alpha0': 0.2}, {'alpha0': 0.3}]
+    parameters = [{'alpha0': -0.2, 'alpha2': -0.2}, {'alpha0': -0.1, 'alpha2': -0.1}, {'alpha0': 0.0, 'alpha2': 0.0},
+                  {'alpha0': 0.1, 'alpha2': 0.1}, {'alpha0': 0.2, 'alpha2': 0.2}, {'alpha0': 0.3, 'alpha2': 0.3}]
     fk_models = {}
     for param in parameters:
         fk_models[str(param)] = ssm.GuidedPF(ssm=TwoLegModel(**param), data=y)
-    compare_parameters(fk_models=fk_models, true_states=x, data=y, dt=dt, nb_particles=N, nb_runs=nb_runs,
-                       t_trunc=t_trunc, show_fig=show_fig, export_name=export_name)
+    compare_parameters(fk_models, x, y, dt, N, nb_runs, t_trunc, show_fig, export_name)
